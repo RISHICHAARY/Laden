b0c300a12808b42e8d3b031dcddf170f6fa5f383
Rishi <rs3715@srmist.edu.in>
2022-12-17 12:01:04 +0530

    Test

--- MY_WEB/settings.py
+++ MY_WEB/settings.py
@@ -136,5 +136,7 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CSRF_TRUSTED_ORIGINS = ['https://*.domain.name']
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
